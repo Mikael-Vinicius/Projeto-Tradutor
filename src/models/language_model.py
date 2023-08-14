d22b6d5ecfc2bec2c43c79f0a4d6ecefe87432f7
Mikael-Vinicius <mikaelvinimarques@hotmail.com>
2023-08-14 14:43:10 -0300

    começando o projeto + primeiro requisito feito

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,11 +1,12 @@
-# from .abstract_model import AbstractModel
-# from database.db import db
+from .abstract_model import AbstractModel
+from database.db import db
 
 
 # Req. 1
-class LanguageModel:
-    def __init__(self):
-        raise NotImplementedError
+class LanguageModel(AbstractModel):
+    _collection = db["language"]
+    def __init__(self, data):
+        super().__init__(data)
 
     # Req. 2
     def to_dict(self):
